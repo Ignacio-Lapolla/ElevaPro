4534537f11f35726c267b3ffb153162bb40b1308
Federico Isla <fisla@uade.edu.ar>
2026-05-28 11:43:45 -0300

    add SupervisoresRepository - UsuariosRepository y AppModule

--- app/src/main/java/com/grupo/elevapro/di/AppModule.kt
+++ app/src/main/java/com/grupo/elevapro/di/AppModule.kt
@@ -1,9 +1,19 @@
 package com.grupo.elevapro.di
 
 import com.grupo.elevapro.data.repository.AuthRepository
+import com.grupo.elevapro.data.repository.EmpresaRepository
 import com.grupo.elevapro.data.repository.FakeAuthRepository
+import com.grupo.elevapro.data.repository.FakeEmpresaRepository
 import com.grupo.elevapro.data.repository.FakeOrdenesRepository
+import com.grupo.elevapro.data.repository.FakePermisosRepository
+import com.grupo.elevapro.data.repository.FakePlantillasRepository
+import com.grupo.elevapro.data.repository.FakeSupervisoresRepository
+import com.grupo.elevapro.data.repository.FakeUsuariosRepository
 import com.grupo.elevapro.data.repository.OrdenesRepository
+import com.grupo.elevapro.data.repository.PermisosRepository
+import com.grupo.elevapro.data.repository.PlantillasRepository
+import com.grupo.elevapro.data.repository.SupervisoresRepository
+import com.grupo.elevapro.data.repository.UsuariosRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -14,11 +24,24 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 abstract class AppModule {
 
-    @Binds
-    @Singleton
+    @Binds @Singleton
     abstract fun bindAuthRepository(impl: FakeAuthRepository): AuthRepository
 
-    @Binds
-    @Singleton
+    @Binds @Singleton
     abstract fun bindOrdenesRepository(impl: FakeOrdenesRepository): OrdenesRepository
+
+    @Binds @Singleton
+    abstract fun bindUsuariosRepository(impl: FakeUsuariosRepository): UsuariosRepository
+
+    @Binds @Singleton
+    abstract fun bindSupervisoresRepository(impl: FakeSupervisoresRepository): SupervisoresRepository
+
+    @Binds @Singleton
+    abstract fun bindPlantillasRepository(impl: FakePlantillasRepository): PlantillasRepository
+
+    @Binds @Singleton
+    abstract fun bindEmpresaRepository(impl: FakeEmpresaRepository): EmpresaRepository
+
+    @Binds @Singleton
+    abstract fun bindPermisosRepository(impl: FakePermisosRepository): PermisosRepository
 }
